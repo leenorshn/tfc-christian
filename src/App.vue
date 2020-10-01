e4d0084e36e97ab4e0a9a62fe97f8e61296a5fe1
leenorshn <katembovictor@gmail.com>
2020-10-01 17:46:13 +0100

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <div id="nav">
+      
+    </div> -->
+    <!-- -->
+    <header>
+      <div class="app-title">
+        <h2>
+          ESCO-Admin
+        </h2>
+      </div>
+      <div class="menu">
+        <router-link to="/">Activité</router-link> 
+        <router-link to="/about">Rayon d'action</router-link>
+        <router-link to="/">Agent</router-link> 
+      </div>
+    </header>
+    <div class="main-app">
+ <router-view/>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+*{
+  margin:0px;
+  padding:0px;
+  box-sizing: content-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+header{
+  background-color: #2c3e50;
+  display: flex;
+  color:beige;
+  height: 10vh;
+  justify-content: center;
+  align-items: center;
+}
+.app-title{
+  flex:2;
+}
+.menu{
+  flex:8;
+}
+a{
+  color:white;
+  text-decoration: none;
+  padding: 8px;
+  display: inline-block;
+  width: 140px;
+  border-radius:8px ;
+  background-color:orange;
+  margin:0px 4px;
+}
+
+
 </style>
